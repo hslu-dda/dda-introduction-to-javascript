06407d859104b7ef62d372e9091d5be47243acde
Max Frischknecht <info@maxfrischknecht.ch>
2025-11-03 08:21:38 +0100

    update async example

--- 08-Loading-Data/08_3_Loading-async-await/sketch.js
+++ 08-Loading-Data/08_3_Loading-async-await/sketch.js
@@ -5,19 +5,23 @@ with the new relase of p5js 2.0 you need to work with async/await
 See here: https://beta.p5js.org/reference/p5/async_await/
 */
 
-
-
 let data1;
 let data2;
 
 // use async in front of setup
 async function setup() {
-  createCanvas(800, 400);
+    createCanvas(800, 400);
 
-  // use await instead of .then
-  data1 = await d3.csv("./data/daten-denkmal.csv");
-  data2 = await d3.json("./data/daten-denkmal.json")
+    // use await instead of .then
+    data1 = await d3.csv("./data/daten-denkmal.csv");
+    data2 = await d3.json("./data/daten-denkmal.json");
 
-  console.log(data1, data2)
+    console.log(data1, data2);
+}
 
+function draw() {
+    // still undefined at the beginning.
+    if (data1 && data2.length && data2 && data2.length) {
+        console.log(data1, data2);
+    }
 }
